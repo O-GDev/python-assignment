ccf09e496e68915ba45695e18a84d692f0f6c25e
PapaOGDev <omiwoleoluwagbogo1@gmail.com>
2025-11-16 23:10:00 +0100

    Added rock paper scissors

--- weekendFun/rockPaperScissors.py
+++ weekendFun/rockPaperScissors.py
@@ -1,2 +1,26 @@
 first_player = input("Player 1; Enter \"rock\", \"paper\" or \"scissors\": ")
 second_player = input("Player 2; Enter \"rock\", \"paper\" or \"scissors\": ")
+
+if first_player.lower() == second_player.lower():
+    print("Tie")
+else:
+    if first_player.lower() == "rock":
+        if second_player.lower() == "scissors":
+            print("Player 1 wins")
+        else:
+            print("Player 2 wins")
+
+    elif first_player.lower() == "paper":
+        if second_player.lower() == "rock":
+            print("Player 1 wins")
+        else:
+            print("Player 2 wins")
+
+    elif first_player.lower() == "scissors":
+        if second_player.lower() == "paper":
+            print("Player 1 wins")
+        else:
+            print("Player 2 wins")
+
+    else:
+        print("Invalid input by Player 1")

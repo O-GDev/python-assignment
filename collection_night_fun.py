236fd2a62d347b8cd7596f7a719774193a0d0ca1
PapaOGDev <omiwoleoluwagbogo1@gmail.com>
2026-01-28 01:13:14 +0000

    first commit

---
+++ collection_night_fun.py
@@ -0,0 +1,75 @@
+from unicodedata import name
+
+students=[("Alice",78),("Bob",45),("Charlie",90),("Diana",62),("Kent",30)]
+def print_students(students):
+    for student in students:
+        name,score=student
+        print(name,score)
+        # return(name, score)
+print_students(students)
+
+def student_that_score_above_60(students):
+    student_score_above_60=[student for student in students if student[1]>60]
+    return student_score_above_60
+
+print(student_that_score_above_60(students))
+
+def student_that_passed(students):
+    count=0
+    for student in students:
+        if student[1]>50:
+            count+=1
+    return count
+
+print(student_that_passed(students))
+
+products=[("Laptop",1200),("Mouse",25),("Keyboard",45),("Monitor",300),("USB Cable",10)]
+def product_cost_more_than_100(products):
+    def cost_more_than_100(products):
+        return products[1] > 100
+    product_cost_more_than_100=list(filter(cost_more_than_100,products))
+    return product_cost_more_than_100
+print(product_cost_more_than_100(products))
+
+def sum_product_cost(products):
+    sum=0
+    for product in products:
+        sum+=product[1]
+    return sum
+print(sum_product_cost(products))
+
+def unpack_products_and_print(products):
+    for product in products:
+        product_name, product_price=product
+        print(product_name,product_price)
+    return
+unpack_products_and_print(products)
+
+points = [ (2, 3),(-1, 4),(5, -6),(0, 7),(-3, -2)]
+
+def filter_the_positive_point(points):
+    positive_points=[]
+    for point in points:
+        x_point, y_point=point
+        if x_point > 0 and y_point > 0:
+            positive_points.append((x_point, y_point))
+    return positive_points
+print(filter_the_positive_point(points))
+
+employees = [("John", "IT", 50000),("Jane", "HR", 45000),("Mike", "IT", 60000),("Sara", "Finance", 70000)]
+def filter_employees_list(employees):
+    it_employees=[]
+    it_employees_above_5500=[]
+    for employee in employees:
+        name, department, salary=employee
+        if department=="IT":
+            it_employees.append((name, department, salary))
+    for employee in it_employees:
+        name, department, salary=employee
+        if salary>55000:
+            it_employees_above_5500.append((name, department, salary))
+    return it_employees_above_5500
+print(filter_employees_list(employees))
+
+
+
--- pizza_ordering/PizzaOrderingApp.java
+++ pizza_ordering/PizzaOrderingApp.java
@@ -1,85 +1,91 @@
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class PizzaOrderingApp {
+    public class PizzaOrderingApp {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        String priceTable =
-                "----------------------------------------------------\n" +
-                "|Pizza Type   |Number of Slices   |Price Per Box   |\n" +
-                "----------------------------------------------------\n" +
-                "|Sapa Size    |4                  |2,000           |\n" +
-                "----------------------------------------------------\n" +
-                "|Small Money  |6                  |2,400           |\n" +
-                "----------------------------------------------------\n" +
-                "|Big Boys     |8                  |3,000           |\n" +
-                "----------------------------------------------------\n" +
-                "|Odogwu       |12                 |4,200           |\n" +
-                "----------------------------------------------------";
+            String priceTable =
+                    "----------------------------------------------------\n" +
+                    "|Pizza Type   |Number of Slices   |Price Per Box   |\n" +
+                    "----------------------------------------------------\n" +
+                    "|Sapa Size    |4                  |2,000           |\n" +
+                    "----------------------------------------------------\n" +
+                    "|Small Money  |6                  |2,400           |\n" +
+                    "----------------------------------------------------\n" +
+                    "|Big Boys     |8                  |3,000           |\n" +
+                    "----------------------------------------------------\n" +
+                    "|Odogwu       |12                 |4,200           |\n" +
+                    "----------------------------------------------------";
 
-        System.out.println(priceTable);
+            System.out.println(priceTable);
 
-        Scanner input = new Scanner(System.in);
+            Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the number of people: ");
-        int numberOfPeople = input.nextInt();
-        input.nextLine();
+            System.out.print("Enter the number of people: ");
+            int numberOfPeople = input.nextInt();
+            input.nextLine();
 
-        System.out.print("What type of pizza do you want?: ");
-        String pizzaType = input.nextLine().toLowerCase();
+            System.out.print("What type of pizza do you want?: ");
+            String pizzaType = input.nextLine().toLowerCase();
 
-        int numberOfBoxesToBuy = 0;
-        int leftOver = 0;
-        int customerCost = 0;
-        int overallSlices = 0;
-        int price = 0;
+            int numberOfBoxesToBuy = 0;
+            int leftOver = 0;
+            int customerCost = 0;
+            int overallSlices = 0;
+            int price = 0;
 
-        if (pizzaType.equals("sapa size")) {
-            price = 2000;
-            numberOfBoxesToBuy = numberOfPeople / 4;
-            if (numberOfPeople % 4 != 0) {
-                numberOfBoxesToBuy += 1;
-                leftOver = 4 - (numberOfPeople % 4);
+
+            if (pizzaType.equals("sapa size")) {
+                price = 2000;
+                numberOfBoxesToBuy = numberOfPeople / 4;
+                if (numberOfPeople % 4 != 0) {
+                    numberOfBoxesToBuy += 1;
+                    leftOver = 4 - (numberOfPeople % 4);
+                }
+                customerCost = numberOfBoxesToBuy * price;
+                overallSlices = numberOfBoxesToBuy * 4;
             }
-            customerCost = numberOfBoxesToBuy * price;
-            overallSlices = numberOfBoxesToBuy * 4;
-        }
-        if (pizzaType.equals("small money")) {
-            price = 2400;
-            numberOfBoxesToBuy = numberOfPeople / 6;
-            if (numberOfPeople % 6 != 0) {
-                numberOfBoxesToBuy += 1;
-                leftOver = 6 - (numberOfPeople % 6);
+            if (pizzaType.equals("small money")) {
+                price = 2400;
+                numberOfBoxesToBuy = numberOfPeople / 6;
+                if (numberOfPeople % 6 != 0) {
+                    numberOfBoxesToBuy += 1;
+                    leftOver = 6 - (numberOfPeople % 6);
+                }
+                customerCost = numberOfBoxesToBuy * price;
+                overallSlices = numberOfBoxesToBuy * 6;
             }
-            customerCost = numberOfBoxesToBuy * price;
-            overallSlices = numberOfBoxesToBuy * 6;
-        }
-        if (pizzaType.equals("big boys")) {
-            price = 3000;
-            numberOfBoxesToBuy = numberOfPeople / 8;
-            if (numberOfPeople % 8 != 0) {
-                numberOfBoxesToBuy += 1;
-                leftOver = 8 - (numberOfPeople % 8);
+            if (pizzaType.equals("big boys")) {
+                price = 3000;
+                numberOfBoxesToBuy = numberOfPeople / 8;
+                if (numberOfPeople % 8 != 0) {
+                    numberOfBoxesToBuy += 1;
+                    leftOver = 8 - (numberOfPeople % 8);
+                }
+                customerCost = numberOfBoxesToBuy * price;
+                overallSlices = numberOfBoxesToBuy * 8;
             }
-            customerCost = numberOfBoxesToBuy * price;
-            overallSlices = numberOfBoxesToBuy * 8;
-        }
-        if (pizzaType.equals("odogwu")) {
-            price = 4200;
-            numberOfBoxesToBuy = numberOfPeople / 12;
-            if (numberOfPeople % 12 != 0) {
-                numberOfBoxesToBuy += 1;
-                leftOver = 12 - (numberOfPeople % 12);
+            if (pizzaType.equals("odogwu")) {
+                price = 4200;
+                numberOfBoxesToBuy = numberOfPeople / 12;
+                if (numberOfPeople % 12 != 0) {
+                    numberOfBoxesToBuy += 1;
+                    leftOver = 12 - (numberOfPeople % 12);
+                }
+                customerCost = numberOfBoxesToBuy * price;
+                overallSlices = numberOfBoxesToBuy * 12;
             }
-            customerCost = numberOfBoxesToBuy * price;
-            overallSlices = numberOfBoxesToBuy * 12;
-        }
-
-        System.out.println("Number of boxes of pizza to buy = " + numberOfBoxesToBuy + " boxes (explanation: " + pizzaType + " contains " + numberOfBoxesToBuy + " boxes are enough for " + numberOfPeople + " people as they contain " + overallSlices + " total slices).");
-
-        System.out.println("Number of leftover slices = " + leftOver + " slices (explanation: After serving " + numberOfPeople + " people, you will have " + leftOver + " slices left).");
-
-        System.out.println("Price = " + customerCost + " (explanation: " + price + " per box × " + numberOfBoxesToBuy + " boxes).");
+else{
+        System.out.print("Invalid input!");
     }
-}
+
+            
+        
+            System.out.println("Number of boxes of pizza to buy = " + numberOfBoxesToBuy + " boxes (explanation: " + pizzaType + " contains " + numberOfBoxesToBuy + " boxes are enough for " + numberOfPeople + " people as they contain " + overallSlices + " total slices).");
+
+            System.out.println("Number of leftover slices = " + leftOver + " slices (explanation: After serving " + numberOfPeople + " people, you will have " + leftOver + " slices left).");
+
+            System.out.println("Price = " + customerCost + " (explanation: " + price + " per box × " + numberOfBoxesToBuy + " boxes).");
+        }
+    }
 
